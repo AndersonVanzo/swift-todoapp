02a347cf7596633f74e870d085865831692ed2bd
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-03-25 18:31:49 -0300

    initial commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Anderson Vanzo on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
